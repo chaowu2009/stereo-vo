7305c9ed513482a1bdd8dd03237066cd4ff850b5
Chao Wu <chaowu2009@gmail.com>
2016-12-13 13:00:16 -0500

    fused left + right camera

--- src/visodo.cpp
+++ src/visodo.cpp
@@ -37,6 +37,35 @@ using namespace std;
 #define PL std::setprecision(3)
 
 
+void getPosition_Orientation(string line, float vout[3], Mat &Rout ){
+	// If possible, always prefer std::vector to naked array
+  std::vector<float> v;
+
+  // Build an istream that holds the input string
+  std::istringstream iss(line);
+
+  // Iterate over the istream, using >> to grab floats
+  // and push_back to store them in the vector
+  std::copy(std::istream_iterator<float>(iss),
+        std::istream_iterator<float>(),
+        std::back_inserter(v));
+
+  vout[0] = v[3];
+  vout[1] = v[7];
+  vout[2] = v[11];
+
+  Rout.at<double>(0,0) = v[0];
+  Rout.at<double>(0,1) = v[1];
+  Rout.at<double>(0,2) = v[2];
+  Rout.at<double>(1,0) = v[4];
+  Rout.at<double>(1,1) = v[5];
+  Rout.at<double>(1,2) = v[6];
+  Rout.at<double>(2,0) = v[8];
+  Rout.at<double>(2,1) = v[9];
+  Rout.at<double>(2,2) = v[10];
+  
+}
+
 void getPosition(string line, float vout[3]){
 	// If possible, always prefer std::vector to naked array
   std::vector<float> v;
@@ -76,6 +105,7 @@ int main(int argc, char** argv) {
 
     std::string line; 
     float truthPosition[3] ;
+    Mat truthOrientation;
 
     getline(infile, line);
     getPosition(line, truthPosition);
@@ -121,12 +151,15 @@ int main(int argc, char** argv) {
 
 	Mat traj = Mat::zeros(600, 600, CV_8UC3);
 	Mat trajTruth = Mat::zeros(600, 600, CV_8UC3);
-	Mat currImage_c;
+	Mat currImage_lc;
     Mat currImage_rc; 
 
     string fileFolder = "/home/cwu/Downloads/dataset/sequences/00/"; 
-    Mat R_f_left, t_f_left, previous_img_left;
-	vector<Point2f> previous_feature_left, current_feature_left;
+    Mat R_f_left, t_f_left;
+    Mat previous_img_left = img_2;
+
+	vector<Point2f> previous_feature_left = points2;
+	vector<Point2f> current_feature_left;
 
 	Mat R_f_right, t_f_right, previous_img_right;
 	vector<Point2f> previous_feature_right, current_feature_right;
@@ -135,40 +168,24 @@ int main(int argc, char** argv) {
 	sprintf(filename1, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 0);
 	sprintf(filename2, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 0);
 
+    computeInitialStereoPose("/home/cwu/Downloads/dataset/sequences/00/",
+	                    R_f_left, 
+	                    t_f_left, 
+	                    previous_img_left,
+		                previous_feature_left,
+		                R_f_right, 
+	                    t_f_right, 
+		                previous_img_right,
+		                previous_feature_right);
 
-	//read the first two frames from the dataset
-	Mat current_img_left  = imread(filename1);
-	Mat current_img_right = imread(filename2);
-    
-    previous_img_left = current_img_left;
-    previous_img_right = current_img_right;
-
-	if (!current_img_left.data || !current_img_right.data) {
-	    std::cout << " --(!) Error reading images " << std::endl;
-	}
-
-	// we work with grayscale images
-	cvtColor(current_img_left, current_img_left, COLOR_BGR2GRAY);
-	cvtColor(current_img_right, current_img_right, COLOR_BGR2GRAY);
-	//vector < Point2f > current_feature_left; //vectors to store the coordinates of the feature points
-	featureDetection(current_img_left, current_feature_left);
-
-	//vector < Point2f > current_feature_right; //vectors to store the coordinates of the feature points
-	featureDetection(current_img_right, current_feature_right);
-
-    previous_feature_left = current_feature_left;
-    previous_feature_right = current_feature_right;
-	//stereoVisionIntialPose(fileFolder, 
-	//	                   img_left, feature_left, 
-	//	                   img_right, feature_right);
 
 	for (int numFrame = 2; numFrame < MAX_FRAME; numFrame++) {
 		sprintf(filename, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", numFrame);
-		currImage_c = imread(filename);
+		currImage_lc = imread(filename);
 		getline(infile, line);
         getPosition(line, truthPosition);
 
-#if 1
+#if 0
 		//scale = getAbsoluteScale(numFrame, 0, t_f.at<double>(2));
 		//cout << "scale is " << scale << endl;
 
@@ -179,16 +196,20 @@ int main(int argc, char** argv) {
 			       R_f, t_f);
 
 #else
-       currImage_c = imread(filename);
-
+       currImage_lc = imread(filename);
+       currImage_rc = imread(filename);
        stereoVision(fileFolder,  
                     numFrame, 
-                    currImage_c, 
+                    currImage_lc, 
 	                currImage_rc,
 	                previous_img_left,  previous_feature_left,  current_feature_left, 
        	            previous_img_right, previous_feature_right, current_feature_right,
        	            R_f, t_f); 
 #endif
+       
+       if ( numFrame == 1000){
+       	//  R_f = truthOrientation;
+       }
 
        // for plotting purpose
     	double x1 = t_f.at<double>(0);
@@ -215,7 +236,7 @@ int main(int argc, char** argv) {
         
         cout << "vo: x = " << PL<< x1 << "\t y = " << PL<< y1 << "\t z = " << PL<< z1 << endl;
         cout << "tr: x = " << PL<< truthPosition[0] << "\t y = " << PL<< truthPosition[1] << "\t z = " << PL<< truthPosition[2] << endl;
-		imshow("Road facing camera", currImage_c);
+		imshow("Road facing camera", currImage_lc);
 		imshow("Trajectory", traj);
 		//imshow("Trajectory", trajTruth);
 
--- src/vo_features.h
+++ src/vo_features.h
@@ -143,10 +143,8 @@ void computeInitialPose(string folder,
 	Mat img_1;
 	char filename1[200];
 	char filename2[200];
-	sprintf(filename1,
-			"/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 0);
-	sprintf(filename2,
-			"/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 1);
+	sprintf(filename1, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 0);
+	sprintf(filename2, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 1);
 
 	//read the first two frames from the dataset
 	Mat img_1_c = imread(filename1);
@@ -179,6 +177,88 @@ void computeInitialPose(string folder,
 	t_f = t.clone();
 
 }
+
+void computeInitialStereoPose(string folder, 
+	                    Mat &R_f_left, 
+	                    Mat &t_f_left, 
+	                    Mat &img_left,
+		                vector<Point2f> &points_left,
+		                Mat &R_f_right, 
+	                    Mat &t_f_right, 
+		                Mat &img_right,
+		                vector<Point2f> &points_right) {
+    // left camera
+	Mat img_1;
+	char filename1[200];
+	char filename2[200];
+	sprintf(filename1, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 0);
+	sprintf(filename2,	"/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", 1);
+
+	//read the first two frames from the dataset
+	Mat img_1_c = imread(filename1);
+	Mat img_2_c = imread(filename2);
+
+	if (!img_1_c.data || !img_2_c.data) {
+		std::cout << " --(!) Error reading images " << std::endl;
+	}
+
+	// we work with grayscale images
+	cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
+	cvtColor(img_2_c, img_left, COLOR_BGR2GRAY);
+
+	// feature detection, tracking
+	vector < Point2f > points1; //vectors to store the coordinates of the feature points
+	featureDetection(img_1, points1);        //detect features in img_1
+	vector < uchar > status;
+	featureTracking(img_1, img_left, points1, points_left, status); //track those features to img_2
+
+	//TODO: add a fucntion to load these values directly from KITTI's calib files
+	// WARNING: different sequences in the KITTI VO dataset have different intrinsic/extrinsic parameters
+	double focal = 718.8560;
+	cv::Point2d pp(607.1928, 185.2157);
+	//recovering the pose and the essential matrix
+	Mat E, R, t, mask;
+	E = findEssentialMat(points_left, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
+	recoverPose(E, points_left, points1, R, t, focal, pp, mask);
+
+	R_f_left = R.clone();
+	t_f_left = t.clone();
+
+
+   // right camera
+	
+	sprintf(filename1, "/home/cwu/Downloads/dataset/sequences/00/image_1/%06d.png", 0);
+	sprintf(filename2,	"/home/cwu/Downloads/dataset/sequences/00/image_1/%06d.png", 1);
+
+	//read the first two frames from the dataset
+	img_1_c = imread(filename1);
+	img_2_c = imread(filename2);
+
+	if (!img_1_c.data || !img_2_c.data) {
+		std::cout << " --(!) Error reading images " << std::endl;
+	}
+
+	// we work with grayscale images
+	cvtColor(img_1_c, img_1, COLOR_BGR2GRAY);
+	cvtColor(img_2_c, img_right, COLOR_BGR2GRAY);
+
+	// feature detection, tracking
+	//vector < Point2f > points1; //vectors to store the coordinates of the feature points
+	featureDetection(img_1, points1);        //detect features in img_1
+	//vector < uchar > status;
+	featureTracking(img_1, img_left, points1, points_right, status); //track those features to img_2
+
+	//recovering the pose and the essential matrix
+	//Mat E, R, t, mask;
+	E = findEssentialMat(points_right, points1, focal, pp, RANSAC, 0.999, 1.0, mask);
+	recoverPose(E, points_right, points1, R, t, focal, pp, mask);
+
+	R_f_right = R.clone();
+	t_f_right= t.clone();
+
+
+}
+
 
 void updatePose(char filename[100], 
 	            Mat &prevImage,
@@ -214,7 +294,7 @@ void updatePose(char filename[100],
 	//scale = getAbsoluteScale(numFrame, 0, t.at<double>(2));
 
 	//cout << "Scale is " << scale << endl;
-	double scale = 0.95;
+	double scale = 0.90;
 
 	if ((scale > 0.1) && (t.at<double>(2) > t.at<double>(0))
 			&& (t.at<double>(2) > t.at<double>(1))) {
@@ -267,7 +347,7 @@ void stereoVision(string folder,
 
 	double focal = 718.8560;
 	cv::Point2d pp(607.1928, 185.2157);
-	double scale = 1.0;
+	double scale = 0.95;
 
 	char filename1[200], filename2[200];
 	sprintf(filename1, "/home/cwu/Downloads/dataset/sequences/00/image_0/%06d.png", numFrame);
@@ -289,37 +369,61 @@ void stereoVision(string folder,
 	cvtColor(current_img_left, current_img_left, COLOR_BGR2GRAY);
 	cvtColor(current_img_right, current_img_right, COLOR_BGR2GRAY);
 
-	// feature detection, tracking
-	//vector < Point2f > current_feature_left; //vectors to store the coordinates of the feature points
-	//featureDetection(current_img_left, current_feature_left);
-
-	//vector < Point2f > current_feature_right; //vectors to store the coordinates of the feature points
-	//featureDetection(current_img_right, current_feature_right);
-
 	// left camera feature tracking
 	vector < uchar > status;
 	featureTracking(previous_img_left, current_img_left, previous_feature_left,	current_feature_left, status);
-
-	// right camera feature tracking
-	featureTracking(previous_img_right, current_img_right, previous_feature_right, current_feature_right, status);
-
-	// left pose estimation
+    
+    // left pose estimation
 	Mat E1, R1, t1, mask1;
 	E1 = findEssentialMat(current_feature_left, previous_feature_left, focal, pp, RANSAC, 0.999,	1.0, mask1);
 	recoverPose(E1, current_feature_left, previous_feature_left, R1, t1, focal, pp, mask1);
 
+
+	// right camera feature tracking
+	featureTracking(previous_img_right, current_img_right, previous_feature_right, current_feature_right, status);	
     // right pose estimation
     Mat E2, R2, t2, mask2;
     E2 = findEssentialMat(current_feature_right, previous_feature_right, focal, pp, RANSAC, 0.999,	1.0, mask2);
 	recoverPose(E2, current_feature_right, previous_feature_right, R2, t2, focal, pp, mask2);
-		
-		// fuse left and right pose
+
+    // fuse left and right pose
 		
 	Mat t = (t1+t2)/2.0;
-	Mat R = (R1 + R2)/2.0;
-	t = t1;
-	R = R1;
+	Mat R = R2 ;
 
+	Mat detlaR = R2.inv()*R1;
+	// normalize R
+	double sum = 0;
+	int cols = R.cols, rows= R.rows;
+
+	// enlarge the diagnoal term
+	for (int i = 0; i < rows; i++){
+		double * Mp = detlaR.ptr<double>(i);
+		for (int j = 0; j < cols; j++){
+			if (i==i) {
+		    	Mp[j] = Mp[i]*2;
+		    }
+		}
+    }
+   
+   for (int i = 0; i < rows; i++){
+		const double * Mp = detlaR.ptr<double>(i);
+		for (int j = 0; j < cols; j++){
+	        sum += Mp[j]*Mp[j];
+		}
+    }
+   double sqrt_of_sum = sqrt(sum);
+
+   // Normalize
+   for (int i = 0; i < rows; i++){
+		double * Mp = detlaR.ptr<double>(i);
+		for (int j = 0; j < cols; j++){
+			Mp[j] = Mp[j]/sqrt_of_sum;
+		}
+    }
+   
+ //  R= R1 * detlaR;
+     scale = 0.8;
 	if ( (t.at<double>(2) > t.at<double>(0)) && (t.at<double>(2) > t.at<double>(1))) {
 	    t_f = t_f + scale * (R_f * t);
 		R_f = R * R_f;
@@ -342,7 +446,7 @@ void stereoVision(string folder,
 
 	previous_img_right = current_img_right.clone();
 	previous_feature_right= current_feature_right;
-   
+
 }
 
 void stereoPoseUpdate(Mat &R_f, Mat &t_f, vector<Point2f> &previous_feature,
