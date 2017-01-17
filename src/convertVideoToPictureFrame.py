008cc732892d36429fff5c3d682d2fe174ab6dfc
Chao Wu <chaowu2009@gmail.com>
2017-01-17 09:44:33 -0500

    handle two videos

--- src/convertVideoToPictureFrame.py
+++ src/convertVideoToPictureFrame.py
@@ -1,11 +1,15 @@
 import cv2
-vidcap = cv2.VideoCapture('video_rightCamera.avi')
+
+videoFolder = "/home/cwu/project/dataset/images/5/" 
+
+vidcap = cv2.VideoCapture(videoFolder + 'video_rightCamera.avi')
 success,image = vidcap.read()
 count = 1
 success = True
-imgFolder = "/home/cwu/project/dataset/images/4/img_right/"
+imgFolder = videoFolder + "img_right/" 
 while success:
   success,image = vidcap.read()
   print 'Read a new frame: ', success
   cv2.imwrite(imgFolder +"%d.jpg" % count, image)     # save frame as JPEG file
   count += 1
+print("total ", count -1, " frame")  
--- src/visodo.cpp
+++ src/visodo.cpp
@@ -11,7 +11,7 @@
 using namespace cv;
 using namespace std;
 
-#define MAX_FRAME 1001
+
 //#define MIN_NUM_FEAT 200
 #define PLOT_COLOR CV_RGB(0, 0, 0)
 #define PL std::setprecision(3)
@@ -40,6 +40,8 @@ int RIGHT = 1;
 
 int main(int argc, char** argv) {
 	
+   int MAX_FRAME = 1000;
+	    
 	float q[4];
 	q[0] = 1.0; q[1] = 0.0; q[2] = 0.0; q[3] = 0.0;
 	Mat dcm;
@@ -53,11 +55,12 @@ int main(int argc, char** argv) {
 
 #ifdef __linux__ 
     //linux code goes here
-//    string localDataDir = "/home/cwu/Downloads";
-    std::string localDataDir = "/home/cwu/project/dataset/images/2/";
     std::string resultFile = "/home/cwu/project/stereo-vo/src/vo_result.txt";
-    //cout << "localDataDir is " << localDataDir << endl;
-    std::string imgDir="/home/cwu/project/dataset/images/4/";
+    std::string imgDir="/home/cwu/project/dataset/images/5/";
+    std::string imgFormat = ".jpg";
+    
+    MAX_FRAME = 754;
+    
 	std::string quaternionFile = imgDir + "q.txt";
 	std::fstream infile(quaternionFile.c_str());
 	std::string line;
@@ -66,7 +69,7 @@ int main(int argc, char** argv) {
   //  string localDataDir = "d:/vision";
     string resultFile   = "d:/vision/stereo-vo/src/vo_result.txt";
     string imgDir       = "d:/vision/dataset/images/3/";
-    string localDataDir = "d:/vision/dataset/images/3/";
+   
 	std::string quaternionFile = imgDir + "q.txt";
 	std::fstream infile(quaternionFile);
 	std::string line;
@@ -111,8 +114,8 @@ int main(int argc, char** argv) {
         stringstream ss;
         ss << numFrame;
         string idx = ss.str();
-        string leftImg  = imgDir + "img_left/" + idx + ".png";
-        string rightImg = imgDir + "img_right/" + idx + ".png";
+        string leftImg  = imgDir + "img_left/" + idx + imgFormat;
+        string rightImg = imgDir + "img_right/" + idx + imgFormat;
         //cout << "leftImg = " << leftImg << endl;
         Mat imgOut;
         cvtColor(current_img_left, imgOut, COLOR_BGR2GRAY);
@@ -172,8 +175,8 @@ int main(int argc, char** argv) {
     // use the first two images from left camera to compute the init values.
     //loadImage(localDataDir + "/dataset/sequences/00/image_0/000000.png", img_1, currImage_lc);
     //loadImage(localDataDir + "/dataset/sequences/00/image_0/000001.png", img_2, currImage_lc);
-    loadImage(localDataDir + "/img_left/1.png", img_1, currImage_lc);
-    loadImage(localDataDir + "/img_right/1.png", img_2, currImage_lc);
+    loadImage(imgDir + "/img_left/1" + imgFormat, img_1, currImage_lc);
+    loadImage(imgDir + "/img_right/1"+ imgFormat, img_2, currImage_lc);
 
 #endif
 
@@ -205,10 +208,10 @@ int main(int argc, char** argv) {
     //namedWindow("Road facing camera", WINDOW_AUTOSIZE); // Create a window for display.
     namedWindow("Trajectory", WINDOW_AUTOSIZE); // Create a window for display.
 
-    Mat traj = Mat::zeros(600, 600, CV_8UC3);
-    Mat trajTruth = Mat::zeros(600, 600, CV_8UC3);
+    Mat traj = Mat::zeros(640, 480, CV_8UC3);
+    Mat trajTruth = Mat::zeros(640, 480, CV_8UC3);
 
-    string fileFolder = localDataDir + "/dataset/sequences/00/"; 
+    string fileFolder = imgDir + "/dataset/sequences/00/"; 
     Mat R_f_left, t_f_left;
     previous_img_left = img_2;
 
@@ -232,12 +235,12 @@ int main(int argc, char** argv) {
     //loadImage(localDataDir + "/dataset/sequences/00/image_0/000000.png", previous_img_left, currImage_lc );
     //loadImage(localDataDir + "/dataset/sequences/00/image_0/000001.png", current_img_left,  currImage_lc);
 
-    loadImage(localDataDir + "/img_left/1.png", previous_img_left, currImage_lc );
-    loadImage(localDataDir + "/img_left/2.png", current_img_left,  currImage_lc);
+    loadImage(imgDir + "/img_left/1"+ imgFormat, previous_img_left, currImage_lc );
+    loadImage(imgDir + "/img_left/2" +imgFormat, current_img_left,  currImage_lc);
 
     // read the first two iamges from right camera
-    loadImage(localDataDir + "/img_right/1.png", previous_img_right, currImage_rc);
-    loadImage(localDataDir + "/img_right/2.png", current_img_right, currImage_rc);
+    loadImage(imgDir + "/img_right/1"+ imgFormat, previous_img_right, currImage_rc);
+    loadImage(imgDir + "/img_right/2"+ imgFormat, current_img_right, currImage_rc);
 
     rectifyImage(previous_img_left, previous_img_right, previous_img_left, previous_img_right);
     rectifyImage(current_img_left, current_img_right, current_img_left, current_img_right);
@@ -286,14 +289,12 @@ int main(int argc, char** argv) {
 
 #else
 
-        //string filename1 =  combineName(localDataDir + "/dataset/sequences/00/image_0/", numFrame);
-        //string filename2 =  combineName(localDataDir + "/dataset/sequences/00/image_1/", numFrame);
         stringstream ss;
         ss << numFrame;
         string idx = ss.str();
 
-        string filename1 =  localDataDir + "/img_left/" + idx + ".png";
-        string filename2 =  localDataDir + "/img_right/" + idx + ".png";
+        string filename1 =  imgDir + "/img_left/" + idx + imgFormat;
+        string filename2 =  imgDir + "/img_right/" + idx + imgFormat;
 
         loadImage(filename1, current_img_left, currImage_lc);
         loadImage(filename2, current_img_right, currImage_rc);
@@ -324,11 +325,8 @@ int main(int argc, char** argv) {
         double y1 = t_f.at<double>(1);
         double z1 = t_f.at<double>(2);
 
-        int x = int(x1) + 300;
-        int y = int(z1) + 100;
-
-        int xTruth = int(truthPosition[0])+ 300;
-        int yTruth = int(truthPosition[2])+ 100;
+        int x = int(x1) +320;
+        int y = int(z1) +240;
 
         // output to the screen
         cout << "numFrame:" << numFrame <<" vo: x = " << PL<< x1 << "\t y = " << PL<< y1 << "\t z = " << PL<< z1 << endl;
@@ -337,17 +335,16 @@ int main(int argc, char** argv) {
 #endif
         // current point
         circle(traj, Point(x, y), 0.2, CV_RGB(255, 0, 0), 2);
-        rectangle(traj, Point(10,30), Point(550, 50), PLOT_COLOR, CV_FILLED);
+        rectangle(traj, Point(10,30), Point(500, 50), PLOT_COLOR, CV_FILLED);
 
         sprintf(text, "Coordinates: x = %04fm y = %04fm z = %04fm", x1, y1, z1);
         putText(traj, text, textOrg, fontFace, fontScale, Scalar::all(255),	thickness, 8);
 
         // plot them
 #ifndef REAL_TIME
-        imshow("Road facing camera", currImage_lc);
+        imshow("Left camera", currImage_lc);
 #endif
         imshow("Trajectory", traj);
-        //imshow("Trajectory", trajTruth);
 
         // Save the result
         fout << numFrame << "\t";
@@ -356,7 +353,7 @@ int main(int argc, char** argv) {
         waitKey(2);  //microsecond
     }
 
-    imwrite(localDataDir + "/final_map.png", traj);
+    imwrite(imgDir + "/final_map.png", traj);
 
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
